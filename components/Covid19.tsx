bf2d68371c6158971e97dddbebdadd27cc4502c1
KAMI Loïc <ruvice.kami@gmail.com>
2022-07-29 12:57:09 +0100

    - Added covid icons and using them - News is empty
    TIME: 0.5H

--- components/Covid19.tsx
+++ components/Covid19.tsx
@@ -1,62 +1,46 @@
 import React from 'react';
-// import Image from "next/image"
+import Image from "next/image"
+
+import icon5 from '../assets/images/covid_icons/covid-5.svg'
+import icon7 from '../assets/images/covid_icons/covid-7.svg'
+import icon22 from '../assets/images/covid_icons/covid-22.svg'
+import icon23 from '../assets/images/covid_icons/covid-23.svg'
+import icon9 from '../assets/images/covid_icons/covid-9.svg'
 
 const Covid19: React.FC<{}> = ({ }) => {
     return (
         <>
-            <section className="bg-black text-gray-100 py-16 space-y-12">
+            <section className="bg-box-600 text-gray-100 py-16 space-y-12">
                 <div className="container mx-auto text-center">
-                    <h2 className="text-5xl font-bold">Mesures Barrières - Covid19</h2>
-                    <p className="text-gray-400">Libero minima optio qui</p>
+                    {/* <h2 className="text-5xl font-bold">Mesures Barrières - Covid19</h2> */}
+                    {/* <p className="text-gray-400">Libero minima optio qui</p> */}
                 </div>
                 <div className="container mx-auto lg:px-16">
                     <div className="flex flex-col md:flex-row items-center md:items-center lg:items-start">
                         <div className="flex flex-col w-2/3 p-2 py-8 md:pl-8 md:pt-0 ">
-                            <div className="grid grid-cols-2 md:grid-cols-3 gap-4 text-gray-400">
+                            <div className="grid grid-cols-2 gap-4 text-gray-400">
                                 <div className="p-6 flex flex-col justify-center items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="fill-current w-14 h-14" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                    </svg>
-                                    <p className='text-base text-gray-500'>Mesure</p>
+                                    <Image width={128} height={128} src={icon7} alt='covid prevention' />
+                                    {/* <p className='text-base text-gray-500'>Mesure</p> */}
                                 </div>
                                 <div className="p-6 flex flex-col justify-center items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="fill-current w-14 h-14" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                    </svg>
-                                    <p className='text-base text-gray-500'>Mesure</p>
+                                    <Image width={128} height={128} src={icon22} alt='covid prevention' />
+                                    {/* <p className='text-base text-gray-500'>Mesure</p> */}
                                 </div>
                                 <div className="p-6 flex flex-col justify-center items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="fill-current w-14 h-14" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                    </svg>
-                                    <p className='text-base text-gray-500'>Mesure</p>
+                                    <Image width={128} height={128} src={icon23} alt='covid prevention' />
+                                    {/* <p className='text-base text-gray-500'>Mesure</p> */}
                                 </div>
                                 <div className="p-6 flex flex-col justify-center items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="fill-current w-14 h-14" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                    </svg>
-                                    <p className='text-base text-gray-500'>Mesure</p>
-                                </div>
-                                <div className="p-6 flex flex-col justify-center items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="fill-current w-14 h-14" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                    </svg>
-                                    <p className='text-base text-gray-500'>Mesure</p>
-                                </div>
-                                <div className="p-6 flex flex-col justify-center items-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="fill-current w-14 h-14" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                    </svg>
-                                    <p className='text-base text-gray-500'>Mesure</p>
+                                    <Image width={128} height={128} src={icon9} alt='covid prevention' />
+                                    {/* <p className='text-base text-gray-500'>Mesure</p> */}
                                 </div>
                             </div>
                         </div>
-                        <div className="flex justify-center self-stretch w-1/3 md:px-0">
+                        <div className="flex justify-center self-stretch w-full md:w-1/3 md:px-0">
                             <div className="p-6 flex flex-col justify-center items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="fill-current w-24 h-24" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                </svg>
-                                <p className='text-base text-gray-500'>Mesure</p>
+                                <Image width={360} height={360} src={icon5} alt='covid prevention' />
+                                {/* <p className='text-base text-gray-500'>Mesure</p> */}
                             </div>
                         </div>
                     </div>
--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -20,7 +20,7 @@ const News: NextPage<Props> = ({ data }) => {
     <Layout locale={locale as string} title={title} desc={desc}>
       <Header locale={locale as string} title="News" />
       <section className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
-        {
+        {/* {
           data.allArticles.map(
             article => (
               <Link key={article.id} rel="noopener noreferrer" href={`/news/${article.slug}`} title={`/news/${article.slug}`} >
@@ -39,28 +39,6 @@ const News: NextPage<Props> = ({ data }) => {
               </Link>
             )
           )
-        }
-        {/* {
-          [
-            "https://source.unsplash.com/random/1280x720",
-            "https://source.unsplash.com/random/1280x721",
-            "https://source.unsplash.com/random/1280x722",
-            "https://source.unsplash.com/random/1280x723",
-
-          ].map(link => (
-            <Link key={link} rel="noopener noreferrer" href={'/news/article'} >
-              <span className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:cursor-pointer hover:no-underline focus:no-underline lg:grid lg:grid-cols-12">
-                <div className="w-full h-64 sm:h-96 lg:col-span-7">
-                  <Image src={link} width={720} height={420} className="object-cover bg-gray-400 rounded" alt='Blog post image' />
-                </div>
-                <div className="p-6 space-y-2 lg:col-span-5 md:self-end">
-                  <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">Noster tincidunt reprimique ad pro</h3>
-                  <span className="text-xs text-gray-400">28 Juillet, 2022</span>
-                  <p>Ei delenit sensibus liberavisse pri. Quod suscipit no nam. Est in graece fuisset, eos affert putent doctus id.</p>
-                </div>
-              </span>
-            </Link>
-          ))
         } */}
       </section>
     </Layout>
